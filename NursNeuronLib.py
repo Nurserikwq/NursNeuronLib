4361cf8b6944b0e759e51e97cd149cef2b1f6090
Nurserikwq <orynbaevnurserik05@gmail.com>
2025-10-08 17:19:48 +0500

    Fix: Corrected bias initialization in Dense layer

--- NursNeuronLib.py
+++ NursNeuronLib.py
@@ -1,3 +1,7 @@
+import numpy as np
+from nnfs.datasets import spiral_data
+
+
 class NursNeuron:
 
     inputs = [[1,2,3],[4,5,6],[7,8,9]]
@@ -11,6 +15,30 @@ class NursNeuron:
         return self.outputlist
 
 
+class Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-calc = NursNeuron()
-print(calc.neuron(calc.inputs, calc.weights, calc.bias))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+
+class ActivationRelu:
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+
+
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Dense(2, 3)
+activation1 = ActivationRelu()
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+print(activation1.output[:5])
