4a3fcec4b4580d8b6a63e1479758e8942ca38d02
Nurserikwq <orynbaevnurserik05@gmail.com>
2025-10-13 23:50:35 +0500

    Loss

--- NursNeuronLib.py
+++ NursNeuronLib.py
@@ -31,21 +31,57 @@ class ActivationRelu:
     def forward(self, inputs):
 
         self.output = np.maximum(0, inputs)
-/
+
 
 class ActivationSoftMax:
-    e = math.e
-    def forward(self, inputs):
+
+    def foward(self, inputs):
+
+        self.exp_values - np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = self.exp_values / np.sum(self.exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+
+class Loss:
+    def calculate (self, output, y):
+
+        sample_losses = self.forward(output, y)
+
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+    def Loss_CategoricalCrossentropy(Loss):
+
+        def forward(self, y_pred, y_true):
+            samples = len(y_pred)
+            y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+            if len(y_true.shape) == 1:
+                correct_confidences = y_pred_clipped[
+                    range(samples),
+                    y_true
+                ]
+            elif len(y_true.shape) == 2:
+                correct_confidences = np.sum(
+                    y_pred_clipped * y_true,
+                    axis=1
+                )
+            # Losses
+            negative_log_likelihoods = -np.log(correct_confidences)
+            return negative_log_likelihoods
+
+
+    def acc (self, y_pred, y_true):
+        def forward(self, y_pred, y_true):
+            predictions = np.argmax(y_pred, axis=1)
+            if len(y_true.shape) == 2:
+                class_targets = np.argmax(y_true, axis=1)
+            accuracy = np.mean(predictions == class_targets)
+
+            return(accuracy)
 
 
 
 
 
-X, y = spiral_data(samples = 100, classes = 3)
-
-dense1 = Dense(2, 3)
-activation1 = ActivationRelu()
-dense1.forward(X)
-activation1.forward(dense1.output)
-
-print(activation1.output[:5])
