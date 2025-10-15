416d0d76e38674e79f0ad3793fdbacb612db17b5
Nurserikwq <orynbaevnurserik05@gmail.com>
2025-10-16 03:37:59 +0500

    fixing bugs Loss, testing optimization

--- NursNeuronLib.py
+++ NursNeuronLib.py
@@ -35,9 +35,9 @@ class ActivationRelu:
 
 class ActivationSoftMax:
 
-    def foward(self, inputs):
+    def forward(self, inputs):
 
-        self.exp_values - np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = self.exp_values / np.sum(self.exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
@@ -51,35 +51,27 @@ class Loss:
         data_loss = np.mean(sample_losses)
         return data_loss
 
-    def Loss_CategoricalCrossentropy(Loss):
 
-        def forward(self, y_pred, y_true):
-            samples = len(y_pred)
-            y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+class Loss_CategoricalCrossentropy(Loss):
 
-            if len(y_true.shape) == 1:
-                correct_confidences = y_pred_clipped[
-                    range(samples),
-                    y_true
-                ]
-            elif len(y_true.shape) == 2:
-                correct_confidences = np.sum(
-                    y_pred_clipped * y_true,
-                    axis=1
-                )
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
             # Losses
-            negative_log_likelihoods = -np.log(correct_confidences)
-            return negative_log_likelihoods
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
-
-    def acc (self, y_pred, y_true):
-        def forward(self, y_pred, y_true):
-            predictions = np.argmax(y_pred, axis=1)
-            if len(y_true.shape) == 2:
-                class_targets = np.argmax(y_true, axis=1)
-            accuracy = np.mean(predictions == class_targets)
-
-            return(accuracy)
 
 
 
